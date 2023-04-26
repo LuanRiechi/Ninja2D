dc419d93e6dbb468a121cac9f77174166f7f2a99
LuanRiechi <luanriechi@alunos.utfpr.edu.br>
2023-04-26 20:11:03 -0300

    Arrumando textura e posição da camera

--- Ninja2D/Assets/scriptCamera.cs
+++ Ninja2D/Assets/scriptCamera.cs
@@ -7,6 +7,7 @@ public class scriptCamera : MonoBehaviour
     public GameObject pc;
     public float offset_x = 3;
     public float offset_y = 1;
+    private Vector3 posAtualCamera;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +17,18 @@ public class scriptCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 posicaoPc = new Vector3(pc.transform.position.x+offset_x, 
-                                        pc.transform.position.y + offset_y, 
-                                        -10);
-        transform.position = posicaoPc;
+        if (pc != null)
+        {
+            Vector3 posicaoPc = new Vector3(pc.transform.position.x + offset_x,
+                                pc.transform.position.y + offset_y,
+                                -10);
+            posAtualCamera = posicaoPc;
+            transform.position = posicaoPc;
+        }
+        else
+        {
+            transform.position = posAtualCamera;
+        }
         
     }
 }
